e78ed347c413282956ae7892861470e350ad3b57
antares-alpha <silkyway92@gmail.com>
2020-06-26 18:39:10 +0000

    added instance type

--- configurations/regions/capetown.tfvars
+++ configurations/regions/capetown.tfvars
@@ -20,3 +20,7 @@ public_cidr_block3 = "10.0.103.0/24"
 private_cidr_block1 = "10.0.1.0/24"
 private_cidr_block2 = "10.0.2.0/24"
 private_cidr_block3 = "10.0.3.0/24"
+
+
+
+instance_type = "t2.medium"
--- variable.tf
+++ variable.tf
@@ -1,5 +1,7 @@
 variable "region" {}
+
 variable "cidr_block" {}
+
 variable "public_cidr_block1" {}
 variable "public_cidr_block2" {}
 variable "public_cidr_block3" {}
@@ -7,3 +9,5 @@ variable "public_cidr_block3" {}
 variable "private_cidr_block1" {}
 variable "private_cidr_block2" {}
 variable "private_cidr_block3" {}
+
+variable "instance_type" {}
